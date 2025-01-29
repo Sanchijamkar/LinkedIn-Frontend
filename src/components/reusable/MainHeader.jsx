fa49d9e3774fe31cfaa831a63a63946ebbfe7128
6reenhorn <coding.cyrilj777@gmail.com>
2025-01-30 00:02:31 +0800

    Main Header Nav Icons

--- src/components/reusable/MainHeader.jsx
+++ src/components/reusable/MainHeader.jsx
@@ -1,14 +1,20 @@
+import LinkedinLogo from '../../assets/imgs/linkedin-logo.jpg';
+import SearchIcon from '../../assets/imgs/search-icon.jpg';
+import HomePageIcon from '../../assets/imgs/home-page-icon.png';
+import MyNetworkIcon from '../../assets/imgs/myNetwork-icon.jpg';
+import JobsIcon from '../../assets/imgs/jobs-icon.png';
+import MessagingIcon from '../../assets/imgs/messaging-icon.png';
 
 const MainHeader = () => {
     return (
         <div className="main-header w-full">
             <div className="flex gap-1 items-center">
-                <img src="/public/linkedin-logo.jpg" alt="LinkdIn Logo" className="main-header-logo" />
-                <img src="/public/search-icon.jpg" alt="Search Bar Icon" className="main-header-search-icon" />
-                <img src="/public/home-page-icon.png" alt="Home Page Icon" className="main-header-home-icon" />
-                <img src="/public/myNetwork-icon.jpg" alt="My Network Icon" className="main-header-myNetwork-icon" />
-                <img src="/public/jobs-icon.png" alt="Jobs Icon" className="main-header-jobs-icon" />
-                <img src="/public/messaging-icon.png" alt="Messaging Icon" className="main-header-messaging-icon" />
+                <img src={LinkedinLogo} alt="LinkdIn Logo" className="main-header-logo" />
+                <img src={SearchIcon} alt="Search Bar Icon" className="main-header-search-icon" />
+                <img src={HomePageIcon} alt="Home Page Icon" className="main-header-home-icon" />
+                <img src={MyNetworkIcon} alt="My Network Icon" className="main-header-myNetwork-icon" />
+                <img src={JobsIcon} alt="Jobs Icon" className="main-header-jobs-icon" />
+                <img src={MessagingIcon} alt="Messaging Icon" className="main-header-messaging-icon" />
             </div>
         </div>
     );
