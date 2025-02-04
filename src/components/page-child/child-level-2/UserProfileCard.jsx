8f0c80ca8dcce576092869adb1f4d8c066ad11ba
6reenhorn <coding.cyrilj777@gmail.com>
2025-02-04 16:53:24 +0800

    User Profile Info Update

--- src/components/page-child/child-level-2/UserProfileCard.jsx
+++ src/components/page-child/child-level-2/UserProfileCard.jsx
@@ -1,5 +1,6 @@
 import UserProfilePhoto from "../../../assets/imgs/profile-photo.png";
 import UserProfileCardOverlay from "../../../assets/imgs/user-background-overlay.jpg";
+import USTPLogo from "../../../assets/imgs/ustp-logo.png";
 
 const UserProfileCard = () => {
     return (
@@ -9,7 +10,12 @@ const UserProfileCard = () => {
             <div className="user-profile-card-info flex flex-col items-start justify-center gap-1 p-2 ps-3 pt-4">
                 <img src={UserProfilePhoto} alt="Profile Photo" />
                 <h2 className="mt-0">John Cyril Espina</h2>
-                <p>Student at University of Science and Technology of Southern Philippines</p>
+                <p className="user-profile-university">Student at University of Science and Technology of Southern Philippines</p>
+                <p>Cagayan de Oro, Philippines</p>
+                <div className="flex items-center gap-1 user-profile-university-logo">
+                    <img src={USTPLogo} alt="University Logo" />
+                    <p className="mt-2">University of Science and Technology of Southern Philippines</p>
+                </div>
             </div>
         </div>
     );
