5735a6e0f2de30bb4a389d427a970e2319d32f03
6reenhorn <coding.cyrilj777@gmail.com>
2025-02-15 17:35:45 +0800

    My network left panel update

--- src/components/page-child/NetworkPageLeftPanel.jsx
+++ src/components/page-child/NetworkPageLeftPanel.jsx
@@ -1,12 +1,18 @@
 import ManageMyNetwork from "./child-level-2/ManageMyNetwork";
+import AdsLinkedBanner from "./child-level-2/AdsLinkedInBanner";
+import MainPageFooter from "../reusable/MainPageFooter";
 
 const NetworkPageLeftPanel = () => {
     return (
-        <div className="network-page-left-panel mt-3 p-3"> 
-            <h2 className="manage-my-network pb-3">Manage my network</h2>
-            <div>
-                <ManageMyNetwork /> 
+        <div className="network-page-left-panel"> 
+            <div className="manage-my-network-container mt-3 mb-5 p-3">
+                <h2 className="manage-my-network pb-3">Manage my network</h2>
+                <div>
+                    <ManageMyNetwork />
+                </div>
             </div>
+            <AdsLinkedBanner />
+            <MainPageFooter />
         </div>
     )
 }
