cbb1066bfa4b3a86ee56a41ccafa79e782245d2e
6reenhorn <coding.cyrilj777@gmail.com>
2025-02-27 22:43:56 +0800

    MyNetwork main panel

---
+++ src/components/page-child/NetworkPageRightPanel.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NetworkPageRightPanel = () => {
+    return (
+        <div className='network-page-right-panel'>
+            <div className='network-page-right-panel-selection flex justify-around py-3'>
+                <p>Grow</p>
+                <p>Catch up</p>
+            </div>
+        </div>
+    )
+}
+
+export default NetworkPageRightPanel;
--- src/components/page-level/NetworkPage.jsx
+++ src/components/page-level/NetworkPage.jsx
@@ -1,9 +1,13 @@
 import NetworkPageLeftPanel from "../page-child/NetworkPageLeftPanel";
+import NetworkPageRightPanel from "../page-child/NetworkPageRightPanel";
 
 const NetworkPage = () => {
     return (
         <div className="w-full">
            <NetworkPageLeftPanel />
+           <div>
+               <NetworkPageRightPanel />
+           </div>
         </div>
     );
 }
