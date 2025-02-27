0e31da391e4d6d1c7447188a1617a8c80bdb1ab0
6reenhorn <coding.cyrilj777@gmail.com>
2025-02-27 22:45:07 +0800

    MyNetwork naming convention

--- src/components/page-child/NetworkPageRightPanel.tsx
+++ src/components/page-child/NetworkPageRightPanel.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NetworkPageRightPanel = () => {
     return (
-        <div className='network-page-right-panel'>
-            <div className='network-page-right-panel-selection flex justify-around py-3'>
+        <div className='network-page-main-panel'>
+            <div className='network-page-main-panel-selection flex justify-around py-3'>
                 <p>Grow</p>
                 <p>Catch up</p>
             </div>
